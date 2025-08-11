42aa20e6c4db9380fb2cdcd64e1985529315a095
kokkumong <jeongkh2001@gmail.com>
2025-08-11 16:36:34 +0900

    #5 : Test_Application 구현

--- src/test/java/mission/ApplicationTest.java
+++ src/test/java/mission/ApplicationTest.java
@@ -1,6 +1,5 @@
 package mission;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import api.TestEnvironment;
@@ -10,6 +9,8 @@ import org.junit.jupiter.api.Test;
 public class ApplicationTest extends TestEnvironment {
     @Test
     void testApplication() {
+        run(List.of("봉화군청", "숭실대학교 정보과학관"));
+        assertTrue(output().contains("이동 시간은 2시간 51분으로 예측됩니다."));
     }
 
     @Override
